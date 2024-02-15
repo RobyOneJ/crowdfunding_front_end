f06ea6c0b795f8e14aeb5e21a07d3f7c97379862
roberta de cecco <roberta.dececco@gmail.com>
2024-02-15 21:03:48 +0800

    Added styling to cards; updated NavBar links to be an ordered list; added a NavBar css file for future styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,10 @@ function NavBar() {
     return (
     <div>
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/project">Project</Link>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/project">Project</Link></li>
+            </ul>
         </nav>
         <Outlet />
     </div>  
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,16 +2,19 @@ import { Link } from "react-router-dom";
 import "./ProjectCard.css";
 
 
-function ProjectCard(props) {
-    const{ projectData }= props;
-
+function ProjectCard({ projectData }) {
     return (
         <div className="project-card">
             <Link to="/project">
-                <img src={projectData.image} alt={'${projectData.title} image'}/>
+                <div className="flippable">
+                    <div className="card">
+                        <div className="card-back">{projectData.description}</div>
+                        <div className="card-front"><img src={projectData.image} alt={'${projectData.title} image'} /></div>
+                    </div>
+                </div>
                 <h3>{projectData.title}</h3>
-            </Link>    
-        </div>  
+            </Link>
+        </div>
     );
 
 }

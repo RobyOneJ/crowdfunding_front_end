e964e85140f69443d0a57db677a278a2715de740
roberta de cecco <roberta.dececco@gmail.com>
2024-02-24 10:17:29 +0800

    fix the other footer logos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,8 @@ import { Link, Outlet } from "react-router-dom";
 import useAuth from "../hooks/use-auth.js";
 import "./NavBar.css";
 import githuburl from "../assets/imges/github.png"
+import shecodesurl from "../assets/imges/She_Codes.png"
+import linkedinurl from "../assets/imges/linkedin.png"
 
 function NavBar() {
     const { auth, setAuth } = useAuth();
@@ -25,12 +27,12 @@ function NavBar() {
             <Outlet />
             <footer>
                 <a href="https://www.linkedin.com/in/roberta-de-cecco-837b6a12/" target="_blank" rel="noopener no referrer"><img
-                    className="logo" src="src/assets/imges/linkedin.png" alt="LinkedIn icon" /></a>
+                    className="logo" src={linkedinurl} alt="LinkedIn icon" /></a>
                 <a href="https://github.com/RobyOneJ" target="_blank" rel="noopener no referrer"><img className="logo"
                     src={githuburl} alt="GitHub icon" /></a>
                 <p className="small-text">© 2024 Roberta De Cecco. All rights reserved.</p>
                 <p className="small-text">Empowered by<a href="http://shecodes.com.au" target="_blank" rel="noopener no referrer"><img
-                    className="shecodeslogo" src="src/assets/imges/She_Codes.png" alt="SheCodes icon" />
+                    className="shecodeslogo" src={shecodesurl} alt="SheCodes icon" />
                 </a></p>
             </footer>
         </div>

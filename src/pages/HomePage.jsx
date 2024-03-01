86a224e15fb72c567f46dd56ee2a88bfa9028d18
roberta de cecco <roberta.dececco@gmail.com>
2024-03-01 22:34:28 +0800

    fixed typo on homepage text

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,7 +17,7 @@ function HomePage() {
             <div id='homepage-heading'><b>EDUKidz</b> is a platform that empowers individuals to crowdfund initiatives for underprivileged children, enabling them to access education and pursue their dreams.
                 These children, filled with aspirations for themselves, their families, and their communities, often face financial barriers or geographic limitations that prevent them from attending school. 
                 <p><b>EDUKidz</b> bridges this gap by providing the tools and resources they need to unlock their full potential.</p>
-                <p>Explore the project below, donate to them or create a new one!</p>
+                <p>Explore the projects below, donate to a project or create a new one.</p>
             </div>
             <div id="project-list">
                 {projects.map((projectData, key) => {
